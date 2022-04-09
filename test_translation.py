8eca4fea9066dc5ebb5fc675a47a3d217c29e558
aneesh-shetye <aneeshashetye@gmail.com>
2022-04-09 18:30:57 +0530

    working sweep

--- test_translation.py
+++ test_translation.py
@@ -5,13 +5,20 @@ import os
 
 
 # translation pretraining 
+# sweep translation 
+# wandb sweep_translation.yaml 
 os.system('python ~/context_enhancement/context_enhancement/train_translation.py --load 0')
 
 # context enhancement
+# sweep barlow with translation encoder hyper-params 
+# sweep sweep_barlow.yaml
 os.system('python ~/context_enhancement/context_enhancement/barlow.py --load 1') 
 
 # tranining translation
+#train translation  with translation hyper-params
+#python train_translation.py 
 os.system('python ~/context_enhancement/context_enhancement/train_translation.py --load 1')
 
 # testing translation
+# no need
 os.system('python ~/context_enhancement/context_enhancement/train_translation.py --load 0')
